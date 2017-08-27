0b576bd16532d9c6f2fd332c4d8177b4061c4e2a
Steve Kemp <steve@steve.org.uk>
2017-08-27 18:37:32 +0300

    Added test for verbose version of command

--- cmd_version_test.go
+++ cmd_version_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"runtime"
 	"testing"
 )
 
@@ -16,6 +17,23 @@ func TestVersion(t *testing.T) {
 	expected := "unreleased\n"
 
 	s := versionCmd{}
+	s.Execute(nil, nil)
+	if out.(*bytes.Buffer).String() != expected {
+		t.Errorf("Expected '%s' received '%s'", expected, out)
+	}
+}
+
+func TestVersionVerbose(t *testing.T) {
+	bak := out
+	out = new(bytes.Buffer)
+	defer func() { out = bak }()
+
+	//
+	// Expected
+	//
+	expected := "unreleased\nBuilt with " + runtime.Version() + "\n"
+
+	s := versionCmd{verbose: true}
 	s.Execute(nil, nil)
 	if out.(*bytes.Buffer).String() != expected {
 		t.Errorf("Expected '%s' received '%s'", expected, out)
