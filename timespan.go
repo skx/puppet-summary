11b28481ca6a5335da5541ff45036e2ae5f267d6
Steve Kemp <steve@steve.org.uk>
2017-08-29 07:31:00 +0300

    Moved code around to make it more testable.
    Fixed bug with seconds being /60, when shouldn't have been.

--- timespan.go
+++ timespan.go
@@ -11,6 +11,33 @@ import (
 )
 
 //
+// Describe the given number of seconds
+//
+func timeDescr(seconds int64) string {
+	//
+	// Hacky code to divide that up into human-readable periods.
+	//
+	switch {
+	case seconds < 1:
+		return "just now"
+	case seconds < 2:
+		return "1 second ago"
+	case seconds < 60:
+		return fmt.Sprintf("%d seconds ago", seconds)
+	case seconds < 120:
+		return "1 minute ago"
+	case seconds < 60*60:
+		return fmt.Sprintf("%d minutes ago", seconds/(60))
+	case seconds < 2*60*60:
+		return "1 hour ago"
+	case seconds < 48*60*60:
+		return fmt.Sprintf("%d hours ago", seconds/(60*60))
+	default:
+		return fmt.Sprintf("%d days ago", seconds/(60*60*24))
+	}
+}
+
+//
 // Given a string containing the seconds past the epoch return
 // a human-friendly description of how long ago that was.
 //
@@ -34,26 +61,5 @@ func timeRelative(epoch string) string {
 		ago *= -1
 	}
 
-	//
-	// Hacky code to divide that up into human-readable periods.
-	//
-	switch {
-	case ago < 1:
-		return "just now"
-	case ago < 2:
-		return "1 second ago"
-	case ago < 60:
-		return fmt.Sprintf("%d seconds ago", ago/60)
-	case ago < 120:
-		return "1 minute ago"
-	case ago < 60*60:
-		return fmt.Sprintf("%d minutes ago", ago/(60))
-	case ago < 2*60*60:
-		return "1 hour ago"
-	case ago < 48*60*60:
-		return fmt.Sprintf("%d hours ago", ago/(60*60))
-	default:
-		return fmt.Sprintf("%d days ago", ago/(60*60*24))
-	}
-
+	return (timeDescr(ago))
 }
